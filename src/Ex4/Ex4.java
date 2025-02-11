af947db1fe65e025d413c92f7432c7e0b62f3ba4
João Motta <epjamp@gmail.com>
2025-02-11 01:13:07 -0300

    New JAVA exercises

--- src/Ex4/Ex4.java
+++ src/Ex4/Ex4.java
@@ -1,7 +1,24 @@
 //Crie uma classe para calcular e exibir na tela o peso ideal. IMC = (peso / (altura^2))
-
 package Ex4;
+import javax.swing.JOptionPane;
 
 public class Ex4 {
-    
+    public static void main(String[] args) {
+        
+    //Recebendo as variáveis do usuário
+        String peso = JOptionPane.showInputDialog("Digite o peso: ");
+        String altura = JOptionPane.showInputDialog("Digite a altura: ");
+        
+        int peso_int = Integer.parseInt(peso);
+        int altura_int = Integer.parseInt(altura);
+        
+    //Inserindo as variáveis recebidas na função e recebendo-as na variável IMC
+        double IMC = CalcularPesoIMC(peso_int, altura_int);
+        System.out.println(String.format("O valor desejado de IMC: %.2f",IMC));
+    }
+
+    //Definindo a função CaclularPesoIMC
+        public static double CalcularPesoIMC(int peso, double altura){
+        return (peso/(altura*altura));    
+    }
 }
---
+++ src/Ex5/Ex5.java
@@ -0,0 +1,25 @@
+/*Crie uma classe que receba dois valores numéricos e apresente na tela o resultado das quatro operações básicas: 
+soma, subtração, multiplicação e divisão.*/
+
+package Ex5;
+import javax.swing.JOptionPane;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        String valor1 = JOptionPane.showInputDialog("Insira o primeiro valor: ");
+        String valor2 = JOptionPane.showInputDialog("Insira o segundo valor: ");
+       
+        int valor1_int = Integer.parseInt(valor1);
+        int valor2_int = Integer.parseInt(valor2);
+            
+        Operacoes_basicas(valor1_int, valor2_int);
+    }
+    
+    
+    public static void Operacoes_basicas(int nm1, int nm2){
+        System.out.println(String.format("%d + %d = " + (nm1+nm2), nm1, nm2));
+        System.out.println(String.format("%d - %d = " + (nm1-nm2), nm1, nm2));
+        System.out.println(String.format("%d * %d = " + (nm1*nm2), nm1, nm2));
+        System.out.println(String.format("%d / %d = " + (nm1/nm2), nm1, nm2));
+    }    
+}
